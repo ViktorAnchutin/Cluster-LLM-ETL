5f7fdc4e5b9e34e33673298252d9f6ddd9618ce7
viktoranchutin <anchutin.v.a@gmail.com>
2024-01-25 17:09:42 +0800

    .sh arguments

--- sequential_dag/run_embeddings.sh
+++ sequential_dag/run_embeddings.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 
-sudo docker run -v /home/ubuntu/data/Cluster-LLM-ETL:/app/data viktoranchutin/embeddings data/sequential_dag/transcripts.json 34.224.15.87 80 seq_run
+input_file=$1
+database_address=$2
+database_port=$3
+collection_name=$4
+
+
+sudo docker run -v /home/ubuntu/data/Cluster-LLM-ETL:/app/data viktoranchutin/embeddings data/$input_file $database_address $database_port $collection_name
--- sequential_dag/run_extract.sh
+++ sequential_dag/run_extract.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-sudo docker run -v /home/ubuntu/data/Cluster-LLM-ETL:/app/data viktoranchutin/extract_subtitles:01 data/input/all_videos.txt data/sequential_dag
+input_data=$1
+output_dir=$2
+
+sudo docker run -v /home/ubuntu/data/Cluster-LLM-ETL:/app/data viktoranchutin/extract_subtitles:01 data/$input_data data/$output_dir
