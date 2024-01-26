be8640c56fb8a59677eaecb57b358519a936ad9c
viktoranchutin <anchutin.v.a@gmail.com>
2024-01-26 19:52:54 +0800

    submit from script

--- pipeline.py
+++ pipeline.py
@@ -15,7 +15,11 @@ def main():
     extract_job = create_extract_job()
     emb_job = create_embeddings_job(database_address, database_port, collection_name)
     dag = build_dag(extract_job, emb_job,n_par,videos_path)
-    dags.write_dag(dag, ".")
+    
+    dag_file=dags.write_dag(dag, ".")
+    dag_submit = htcondor.Submit.from_dag(str(dag_file), {'force': 1})
+    schedd = htcondor.Schedd()
+    schedd.submit(dag_submit).cluster()
 
 
 if __name__ == "__main__":
