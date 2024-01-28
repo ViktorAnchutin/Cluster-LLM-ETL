3355a5c5a0140678e9a1f2ce5bc38c39646cd9d2
viktoranchutin <anchutin.v.a@gmail.com>
2024-01-28 18:33:09 +0800

    dag generation description

--- pipeline.py
+++ pipeline.py
@@ -4,6 +4,7 @@ import configparser
 from utils import *
 
 def main():
+    #read config
     config = configparser.ConfigParser()
     config.read('config.ini')
     videos_path = config.get("def",'videos_list')
@@ -12,10 +13,12 @@ def main():
     collection_name=config.get("def",'collection_name')
     n_par = int(config.get('def','parallel_pipelines'))
 
+    #create jobs and dag
     extract_job = create_extract_job()
     emb_job = create_embeddings_job(database_address, database_port, collection_name)
     dag = build_dag(extract_job, emb_job,n_par,videos_path)
     
+    #submit dag
     dag_file=dags.write_dag(dag, ".")
     dag_submit = htcondor.Submit.from_dag(str(dag_file), {'force': 1})
     schedd = htcondor.Schedd()
